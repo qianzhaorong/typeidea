32b678419e15511404c4a778127d08f2aa691bc1
qianzhaorong <7741151370@qq.com>
2019-05-05 18:00:48 +0800

    渲染模板（第一次）

--- typeidea/blog/views.py
+++ typeidea/blog/views.py
@@ -4,12 +4,8 @@ from django.http import HttpResponse
 
 # Create your views here.
 def post_list(request, category_id=None, tag_id=None):
-    content = 'post list category={category_id}, tag_id={tag_id}'.format(
-        category_id=category_id,
-        tag_id=tag_id,
-    )
-    return HttpResponse(content=content)
+    return render(request, 'blog/list.html', context={'name': 'post_list'})
 
 
 def post_detail(request, post_id):
-    return HttpResponse('detail')
+    return render(request, 'blog/detail.html', context={'name': 'post_detail'})
