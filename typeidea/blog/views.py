aab4a27cd2d53a8accc57028648ade3749773c08
qianzhaorong <7741151370@qq.com>
2019-05-05 17:53:48 +0800

    url到view的初次映射

--- typeidea/blog/views.py
+++ typeidea/blog/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+def post_list(request, category_id=None, tag_id=None):
+    content = 'post list category={category_id}, tag_id={tag_id}'.format(
+        category_id=category_id,
+        tag_id=tag_id,
+    )
+    return HttpResponse(content=content)
+
+
+def post_detail(request, post_id):
+    return HttpResponse('detail')
